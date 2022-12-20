247e1c5f300dc02774ddb6e36a675998190ea566
01Nijat <mr.nijat.asgarov@gmail.com>
2022-12-21 01:17:44 +0400

    Hazirlandi

---
+++ ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewComponents
+{
+    public class HeaderViewComponent:ViewComponent
+    {
+        private readonly Context _context;
+        public HeaderViewComponent(Context context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            ViewBag.ProductCount = 0;
+            if (Request.Cookies["basket"]!=null)
+            {
+                int total = 0;
+                
+                List<BasketProduct> products = JsonConvert.DeserializeObject<List<BasketProduct>>(Request.Cookies["basket"]);
+                //foreach (var item in products)
+                //{
+                //    total+=item.Count;
+                //}
+
+                //ViewBag.ProductCount = total ;
+                ViewBag.ProductCount = products.Count;
+            }
+            Bio bio = _context.Bios.FirstOrDefault();
+            return View(await Task.FromResult(bio));
+
+        } 
+    }
+}
---
+++ ViewComponents/ProductsViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewComponents
+{
+    public class ProductsViewComponent:ViewComponent
+    {
+        private readonly Context _context;
+        public ProductsViewComponent(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int take)
+        {
+            List<Product> products = _context.Products
+                .Include(p => p.Category)
+                .Take(take)
+                .ToList();
+            return View(await Task.FromResult(products));
+        }
+    }
+}

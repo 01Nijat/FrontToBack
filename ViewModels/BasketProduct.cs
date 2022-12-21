43dbcfe93257faa5067c77bd2dc587285c1d39a2
01Nijat <mr.nijat.asgarov@gmail.com>
2022-12-22 01:18:16 +0400

    complete

---
+++ ViewModels/BasketProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewModels
+{
+    public class BasketProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ ViewModels/HomeVm.cs
@@ -0,0 +1,24 @@
+﻿using FrontToBack.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.ViewModels
+{
+    public class HomeVm
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public SliderDesc SliderDesc { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public About About { get; set; }
+        public IEnumerable<Expert> Experts { get; set; }
+        public IEnumerable<Blogs> Blogs { get; set; }
+        public IEnumerable<BlogsSlider> BlogsSliders { get; set; }
+        public IEnumerable<InstagramSlider> InstagramSliders { get; set; }
+
+
+
+
+    }
+}
